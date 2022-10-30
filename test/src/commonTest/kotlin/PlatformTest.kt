f1aa220853e0dc847021f1833b32abefbc771913
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2022-10-30 14:41:54 +0100

    runOnPlatforms tests added

--- test/src/commonTest/kotlin/PlatformTest.kt
+++ test/src/commonTest/kotlin/PlatformTest.kt
@@ -4,7 +4,24 @@ import kotlin.test.*
 
 class PlatformTest {
     @Test
-    fun test() {
+    fun testGetPlatform() {
         println("Platform: ${getPlatform()}")
+    }
+
+    @Test
+    fun testRunOnPlatforms1() {
+        runOnPlatforms {
+            fail()
+        }
+    }
+
+    @Test
+    fun testRunOnPlatforms2() {
+        var run = false
+        runOnPlatforms(Platform.Jvm, Platform.Js, Platform.Linux, Platform.Mac) {
+            assertFalse(run)
+            run = true
+        }
+        assertTrue(run)
     }
 }
