772526b262e7801f8981180c69918472c7bb397a
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2022-11-01 08:56:15 +0100

    KtorReadingFromClosedSocketBugTest updated

--- test/src/jvmAndNixTest/kotlin/transport/ktor/KtorReadingFromClosedSocketBugTest.kt
+++ test/src/jvmAndNixTest/kotlin/transport/ktor/KtorReadingFromClosedSocketBugTest.kt
@@ -6,15 +6,20 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.*
 import kotlin.test.*
 
+// see https://youtrack.jetbrains.com/issue/KTOR-5093/Native-Read-from-a-closed-socket-doesnt-throw-an-exception
+
 @Ignore // TODO: remove @Ignore for running tests
 class KtorReadingFromClosedSocketBugTest {
     @Test
-    fun `reading from a closed socket must throw an exception on any target`() {
+    fun `Native - Read from a closed socket doesn't throw an exception`() {
         val address = InetSocketAddress("localhost", 12345)
         val tcp = aSocket(SelectorManager(EmptyCoroutineContext)).tcp()
         val serverSocket = tcp.bind(address)
         runBlocking {
-            launch { serverSocket.accept() }
+            launch {
+                serverSocket.accept()
+                println("client connected")
+            }
             val clientSocket = tcp.connect(address)
             val clientReadChannel = clientSocket.openReadChannel()
             launch {
@@ -22,9 +27,10 @@ class KtorReadingFromClosedSocketBugTest {
                     println("reading from clientSocket ...")
                     clientReadChannel.readByte()
                 } catch (e: Exception) {
-                    println("exception is thrown on jvm, but not on linuxX64 or macosArm64 targets: $e")
+                    println("exception is correctly thrown for jvm but not for native targets")
                 }
             }
+            delay(1_000)
             clientSocket.close()
             println("clientSocket closed")
         }
@@ -35,12 +41,14 @@ class KtorReadingFromClosedSocketBugTest {
 
         jvm target
         ----------
+        client connected
         clientSocket closed
         reading from clientSocket ...
-        exception is thrown on jvm, but not on linuxX64 or macosArm64 targets: kotlinx.coroutines.JobCancellationException: StandaloneCoroutine was cancelled; job="cio-from-nio-reader#4":StandaloneCoroutine{Cancelled}@487db668
+        exception is correctly thrown for jvm but not for native targets
 
-        other targets
-        -------------
+        native targets
+        --------------
+        client connected
         clientSocket closed
         reading from clientSocket ...
         <no exception here, reading from clientSocket never returns>
