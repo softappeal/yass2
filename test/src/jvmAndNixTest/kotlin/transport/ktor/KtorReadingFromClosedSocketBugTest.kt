61cf25c9400243ac3dab77251417e6bde45f0a0a
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2022-11-03 10:32:47 +0100

    KtorReadingFromClosedSocketBugTest updated

--- test/src/jvmAndNixTest/kotlin/transport/ktor/KtorReadingFromClosedSocketBugTest.kt
+++ test/src/jvmAndNixTest/kotlin/transport/ktor/KtorReadingFromClosedSocketBugTest.kt
@@ -4,6 +4,7 @@ import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
 import kotlinx.coroutines.*
 import kotlin.coroutines.*
+import kotlin.random.*
 import kotlin.test.*
 
 // see https://youtrack.jetbrains.com/issue/KTOR-5093/Native-Read-from-a-closed-socket-doesnt-throw-an-exception
@@ -12,7 +13,7 @@ import kotlin.test.*
 class KtorReadingFromClosedSocketBugTest {
     @Test
     fun `Native - Read from a closed socket doesn't throw an exception`() {
-        val address = InetSocketAddress("localhost", 12345)
+        val address = InetSocketAddress("localhost", Random.nextInt(2_000..65_000))
         val tcp = aSocket(SelectorManager(EmptyCoroutineContext)).tcp()
         val serverSocket = tcp.bind(address)
         runBlocking {
@@ -21,13 +22,19 @@ class KtorReadingFromClosedSocketBugTest {
                 println("client connected")
             }
             val clientSocket = tcp.connect(address)
-            val clientReadChannel = clientSocket.openReadChannel()
             launch {
+                val clientReadChannel = clientSocket.openReadChannel()
+                println("clientSocket.isClosed: ${clientSocket.isClosed}")
+                println("clientReadChannel.isClosedForRead: ${clientReadChannel.isClosedForRead}")
+                delay(2_000)
+                println("clientSocket.isClosed: ${clientSocket.isClosed}")
+                println("clientReadChannel.isClosedForRead: ${clientReadChannel.isClosedForRead}")
                 try {
-                    println("reading from clientSocket ...")
+                    println("trying to read from clientReadChannel")
                     clientReadChannel.readByte()
+                    println("reading from clientReadChannel succeeded^")
                 } catch (e: Exception) {
-                    println("exception is correctly thrown for jvm but not for native targets")
+                    println("reading from clientReadChannel failed")
                 }
             }
             delay(1_000)
@@ -35,24 +42,16 @@ class KtorReadingFromClosedSocketBugTest {
             println("clientSocket closed")
         }
         /*
-
-        OUTPUT
-        ======
-
-        jvm target
-        ----------
-        client connected
-        clientSocket closed
-        reading from clientSocket ...
-        exception is correctly thrown for jvm but not for native targets
-
-        native targets
-        --------------
-        client connected
-        clientSocket closed
-        reading from clientSocket ...
-        <no exception here, reading from clientSocket never returns>
-
+            jvm target (expected behavior)              native target (erroneous behavior)
+            ------------------------------              ----------------------------------
+            client connected                            client connected
+            clientSocket.isClosed: false                clientSocket.isClosed: false
+            clientReadChannel.isClosedForRead: false    clientReadChannel.isClosedForRead: false
+            clientSocket closed                         clientSocket closed
+            clientSocket.isClosed: true                 clientSocket.isClosed: false
+            clientReadChannel.isClosedForRead: true     clientReadChannel.isClosedForRead: false
+            trying to read from clientReadChannel       trying to read from clientReadChannel
+            reading from clientReadChannel failed       <reading from clientReadChannel never returns>
         */
     }
 }
