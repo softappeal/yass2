2ff3500be615bf808ecdcf156d32f7548b0139c8
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2023-05-30 08:01:57 +0200

    tests cleanup

--- test/src/jvmTest/kotlin/contract/GenerateTest.kt
+++ test/src/jvmTest/kotlin/contract/GenerateTest.kt
@@ -12,7 +12,7 @@ class GenerateTest {
     fun test() {
         fun verify(fileName: String) {
             val newGenerated = Path("build/generated/ksp/jvm/jvmTest/kotlin/ch/softappeal/yass2/contract/$fileName.kt").readAndFixLines()
-            val oldGenerated = Path("src/jvmTest/kotlin/contract/generated/$fileName.kt").readAndFixLines()
+            val oldGenerated = Path("src/commonTest/kotlin/contract/generated/$fileName.kt").readAndFixLines()
                 .replace("package ch.softappeal.yass2.contract.generated", "package ch.softappeal.yass2.contract")
             check(newGenerated == oldGenerated) {
                 "$fileName is\n${">".repeat(120)}\n$newGenerated${"<".repeat(120)}\nbut should be\n${">".repeat(120)}\n$oldGenerated${"<".repeat(120)}"
