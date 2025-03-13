6d63903e384208f7ac1c3734994eb2f1cb8396e9
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2025-03-13 15:43:51 +0100

    browser tests

--- test/src/wasmJsTest/kotlin/ktor/BrowserTest.kt
+++ test/src/wasmJsTest/kotlin/ktor/BrowserTest.kt
@@ -1,13 +1,12 @@
 package ch.softappeal.yass2.ktor
 
 import io.ktor.client.engine.js.JsClient
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalJsExport::class) @JsExport
 fun remoteTest() {
-    @OptIn(DelicateCoroutinesApi::class) GlobalScope.launch {
+    MainScope().launch {
         ktorClientTest(JsClient())
     }
 }
