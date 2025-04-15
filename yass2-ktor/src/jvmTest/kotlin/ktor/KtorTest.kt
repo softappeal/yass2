414ad9f3e109d5db66bdb4f441cda64cc140cc9f
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2025-04-15 19:12:25 +0200

    dev-2.2.0

--- yass2-ktor/src/jvmTest/kotlin/ktor/KtorTest.kt
+++ yass2-ktor/src/jvmTest/kotlin/ktor/KtorTest.kt
@@ -35,7 +35,7 @@ val Server = embeddedServer(io.ktor.server.cio.CIO, PORT) {
         }
         // code
         staticFiles("/js", File("./build/js/packages/yass2-ktor-test/kotlin"))
-        staticFiles("/wasm", File("./build/js/packages/yass2-ktor-wasm-test/kotlin"))
+        staticFiles("/wasm", File("./build/wasm/packages/yass2-ktor-test/kotlin"))
         // sources
         staticFiles("/src", File("./yass2-ktor/src"))
         staticFiles("/", File("."))
