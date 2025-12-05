6b10e8e25d99402b7471849c9f2845ede0afe064
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2025-12-05 15:26:27 +0100

    forEachSeparator doc added

--- yass2-core/src/commonMain/kotlin/Collections.kt
+++ yass2-core/src/commonMain/kotlin/Collections.kt
@@ -1,5 +1,6 @@
 package ch.softappeal.yass2.core
 
+/** Calls [action] on each element and [separator] between elements. */
 public inline fun <T> Iterable<T>.forEachSeparator(separator: () -> Unit, action: (T) -> Unit) {
     val iterator = iterator()
     if (!iterator.hasNext()) return
