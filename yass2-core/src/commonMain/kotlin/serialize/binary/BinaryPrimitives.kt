ab7f1355e87e8482bcd782d0a2453b7378774eb0
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2026-03-15 08:51:50 +0100

    BinaryInt.kt and BinaryVarInt.kt moved to BinaryPrimitives.kt

--- yass2-core/src/commonMain/kotlin/serialize/binary/BinaryPrimitives.kt
+++ yass2-core/src/commonMain/kotlin/serialize/binary/BinaryPrimitives.kt
@@ -3,6 +3,19 @@ package ch.softappeal.yass2.core.serialize.binary
 import ch.softappeal.yass2.core.serialize.Reader
 import ch.softappeal.yass2.core.serialize.Writer
 
+private const val FALSE: Byte = 0
+private const val TRUE: Byte = 1
+
+public fun Writer.writeBinaryBoolean(value: Boolean) {
+    writeByte(if (value) TRUE else FALSE)
+}
+
+public fun Reader.readBinaryBoolean(): Boolean = when (val b = readByte()) {
+    FALSE -> false
+    TRUE -> true
+    else -> error("unexpected binary boolean $b")
+}
+
 // see https://protobuf.dev/programming-guides/encoding/#varints
 
 private const val INT_INV_7F: Int = 0x7F.inv()
--- yass2-ktor/src/commonTest/kotlin/core/serialize/binary/BinaryPrimitivesTest.kt
+++ yass2-ktor/src/commonTest/kotlin/core/serialize/binary/BinaryPrimitivesTest.kt
@@ -9,7 +9,7 @@ import ch.softappeal.yass2.core.serialize.checkDrained
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-fun <T> check(value: T, write: Writer.(value: T) -> Unit, vararg bytes: Int, read: Reader.() -> T) {
+private fun <T> check(value: T, write: Writer.(value: T) -> Unit, vararg bytes: Int, read: Reader.() -> T) {
     val writer = ByteArrayWriter()
     writer.write(value)
     val byteArray = writer.toyByteArray()
@@ -18,7 +18,7 @@ fun <T> check(value: T, write: Writer.(value: T) -> Unit, vararg bytes: Int, rea
     assertEquals(value, reader.read().apply { reader.checkDrained() })
 }
 
-class BinaryIntTest {
+class BinaryPrimitivesTest {
     @Test
     fun boolean() {
         check(false, { writeBinaryBoolean(it) }, 0) { readBinaryBoolean() }
@@ -26,5 +26,33 @@ class BinaryIntTest {
         assertFailsWithMessage<IllegalStateException>("unexpected binary boolean 2") {
             ByteArrayReader(byteArrayOf(2)).readBinaryBoolean()
         }
+    }
+
+    @Test
+    fun varInt() {
+        fun check(value: Int, vararg bytes: Int) = check(value, { writeVarInt(it) }, *bytes) { readVarInt() }
+        check(0, 0)
+        check(0x7F, 0x7F)
+        check(0x80, 0x80, 0x01)
+        check(0x3F_FF, 0xFF, 0x7F)
+        check(0x40_00, 0x80, 0x80, 0x01)
+        check(-1, 0xFF, 0xFF, 0xFF, 0xFF, 0x0F)
+    }
+
+    @Test
+    fun zigzagInt() {
+        fun check(v: Int, z: Int) {
+            assertEquals(z, v.toZigZag())
+            assertEquals(v, z.fromZigZag())
+        }
+        check(0, 0)
+        check(-1, 1)
+        check(1, 2)
+        check(-2, 3)
+        check(2, 4)
+        check(Int.MIN_VALUE, -1)
+        check(Int.MAX_VALUE, -2)
+        check(Int.MIN_VALUE + 1, -3)
+        check(Int.MAX_VALUE - 1, -4)
     }
 }
