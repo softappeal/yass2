9e02b6d3159fddbe55934d1d46d212c1c12fc8a5
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2026-07-24 06:52:25 +0200

    dependencies of module `yass2-generate` fixed

--- yass2-generate/build.gradle.kts
+++ yass2-generate/build.gradle.kts
@@ -2,7 +2,7 @@ kotlin {
     sourceSets {
         jvmMain {
             dependencies {
-                implementation(project(":yass2-core"))
+                api(project(":yass2-core"))
                 implementation(kotlin("reflect"))
             }
         }
